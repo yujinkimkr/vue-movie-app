3cdc0cdf738811cb510340d4b745b8252a459881
yujinkimkr <kyjin.dv@gmail.com>
2023-04-04 22:26:27 +0200

    Add Netlify functions

--- src/store/movie.js
+++ src/store/movie.js
@@ -68,10 +68,10 @@ export default {
             })
           }
         }
-      } catch (message) {
+      } catch ({ message }) {
         commit('updateState', {
           movies: [],
-          message: message
+          message
         })
       } finally {
         commit('updateState', {
@@ -108,27 +108,6 @@ export default {
   }
 }
 
-function _fetchMovie(payload) {
-  const { title, type, year, page, id } = payload;
-  const OMDB_API_KEY = '7035c60c';
-  const url = id
-    // id 값이 있을 때
-    ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}`
-    : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`;
-
-
-  return new Promise((resolve, reject) => {
-    axios
-      .get(url)
-      .then(res => {
-        if (res.data.Error) {
-          reject(res.data.Error);
-          return;
-        }
-        resolve(res);
-      })
-      .catch(err => {
-        reject(err.message);
-      });
-  });
+async function _fetchMovie(payload) {
+  return await axios.post('./netlify/functions/movie', payload);
 }
