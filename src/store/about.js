27ae1000b6292103a3c9089401e792155389215a
yujinkimkr <125222287+yujinkimkr@users.noreply.github.com>
2023-05-27 02:25:57 +0200

    Update about.js
    modified github link in About.js

--- src/store/about.js
+++ src/store/about.js
@@ -3,7 +3,7 @@ export default {
   state: () => ({
     name: 'YJKIM',
     email: 'kyjin.dev@gmail.com',
-    blog: 'https://ninetynine-2026.tistory.com/',
+    github: 'https://github.com/yujinkimkr',
     phone: '+46-666-360-277',
     // image: 'https://heropy.blog/css/images/logo.png'
     image: 'https://bangory.org/wp-content/uploads/2016/05/person-icon-233x300.png'
